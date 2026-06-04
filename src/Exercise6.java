c413ef969f33e18cdd062ab8e397555f86d8dd40
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 10:56:11 +0300

    concatenate string literal and user input

---
+++ src/Exercise6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        System.out.println("What's your name ? ");
+        Scanner scanner=new Scanner(System.in);
+        String name= scanner.nextLine();
+        System.out.println("Hi " + name);
+    }
+}
