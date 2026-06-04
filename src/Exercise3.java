1cccf30f5297c95d9409d9b4f8b556d5e4e2f7bc
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 09:56:17 +0300

    capitalised  the letter w under write a message

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        System.out.println("write a message:");
+        System.out.println("Write a message:");
         Scanner scanner= new Scanner(System.in);
         String message= scanner.nextLine();
         System.out.println(message);
