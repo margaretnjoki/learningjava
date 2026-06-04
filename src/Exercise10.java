1e63a776d52ba42f4b0e19ccf4c2063492d7f089
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 18:47:04 +0300

    reading integers

---
+++ src/Exercise10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise10 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter a value");
+        String value= scanner.nextLine();
+        int result=Integer.valueOf(value);
+        System.out.println("you gave the number " + result);
+
+
+
+
+    }
+}
