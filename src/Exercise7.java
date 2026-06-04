5418228d60916752304b67c4c9f85ad7c4101716
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 11:08:15 +0300

    a program for a dialogue

---
+++ src/Exercise7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Greetings! How are you doing?");
+        String name= scanner.nextLine();
+        System.out.println("Oh, how interesting.Tell me more!");
+        String more= scanner.nextLine();
+        System.out.println("Thanks for sharing");
+
+
+    }
+}
