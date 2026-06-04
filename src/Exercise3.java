93ba1382dfb41463d8d027c71c2800c2dcf8e9b7
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 09:53:14 +0300

    Exercise3: code asking  the user input and displaying the input

---
+++ src/Exercise3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        System.out.println("write a message:");
+        Scanner scanner= new Scanner(System.in);
+        String message= scanner.nextLine();
+        System.out.println(message);
+    }
+
+}
