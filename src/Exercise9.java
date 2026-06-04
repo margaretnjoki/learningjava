e3b7e55603747e7684acc6db308dbc27e3302a51
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 18:13:59 +0300

    variable initialization

---
+++ src/Exercise9.java
@@ -0,0 +1,19 @@
+public class Exercise9 {
+    public static void main(String[] args) {
+        int chicken= 3;
+        double  bacon=5.5;
+        String tractor= "none!";
+        System.out.println( "Chicken:");
+        System.out.println( chicken);
+        System.out.println( "Bacon");
+        System.out.println( bacon);
+        System.out.println( "Tractor");
+        System.out.println( "And finally, a summary:");
+        System.out.println(chicken );
+        System.out.println(bacon);
+        System.out.println(tractor);
+
+
+
+    }
+}
