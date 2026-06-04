c0ba358dafcdff5f1ca1e7e7e67325a83d5e6285
margaretnjoki <maggykiroko@gmail.com>
2026-06-04 10:30:54 +0300

    Exercise5: reading user input 3 times using scanner

---
+++ src/Exercise5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.println("Write a message");
+        String message="Hi";
+        Scanner scanner=  new Scanner(message);
+        System.out.println(message);
+        System.out.println(message);
+        System.out.println(message);
+
+
+    }
+}
