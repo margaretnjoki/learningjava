d7fa2f5e88db1227c1f0a8f7ea8294d2993bfcfb
margaretnjoki <maggykiroko@gmail.com>
2026-06-09 15:01:29 +0300

    a program to display the sum of elements in an array

---
+++ src/Exercise57.java
@@ -0,0 +1,12 @@
+public class Exercise57 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 3, 4, 2};
+         int sum=0;
+        for (int num: numbers){
+            sum=sum + num;
+        }
+        System.out.println(sum);
+    }
+
+}
+
