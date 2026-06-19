63a2434a2a48137a2cafa2dd63b463e1e26b0130
margaretnjoki <maggykiroko@gmail.com>
2026-06-19 16:23:52 +0300

    gift workshop

---
+++ src/Package.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Package {
+    private ArrayList<Gift> gifts;
+
+        public Package() {
+            this.gifts = new ArrayList<>();
+        }
+
+
+        public void addGift(Gift gift) {
+            this.gifts.add(gift);
+        }
+
+
+        public int totalWeight() {
+            int sum = 0;
+
+            for (Gift gift : this.gifts) {
+                sum += gift.getWeight();
+            }
+
+            return sum;
+        }
+    }
+
