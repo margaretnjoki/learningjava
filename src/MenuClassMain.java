42a5e8e22bbacab0910fdd7bfedfaeb551f78a7a
margaretnjoki <maggykiroko@gmail.com>
2026-06-19 14:37:51 +0300

    menu printing a list of meals and methods to clear,and add meals

---
+++ src/MenuClassMain.java
@@ -0,0 +1,18 @@
+public class MenuClassMain {
+    public static void main(String[] args) {
+        MenuClass  menu = new MenuClass();
+
+
+        menu.addMeal("Tofu ratatouille");
+        menu.addMeal("Chilli coconut chicken");
+        menu.addMeal("Chilli coconut chicken");
+        menu.addMeal("Meatballs with mustard sauce");
+
+        menu.printMeals();
+        menu.clearMenu();
+
+        System.out.println();
+        menu.addMeal("Tomato and mozzarella salad");
+        menu.printMeals();
+    }
+}
