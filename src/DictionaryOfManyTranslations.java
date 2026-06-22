1ce1e5c6ca29c52c8dededefe7dad0bc9a5c6182
margaretnjoki <maggykiroko@gmail.com>
2026-06-22 10:16:36 +0300

    a hashmap dictionary with one key with more than one value

---
+++ src/DictionaryOfManyTranslations.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+    public class DictionaryOfManyTranslations {
+
+        private HashMap<String, ArrayList<String>> dictionary;
+
+        public DictionaryOfManyTranslations() {
+            this.dictionary = new HashMap<>();
+        }
+
+
+
+        public void add(String word, String translation) {
+            if (!dictionary.containsKey(word)) {
+                dictionary.put(word, new ArrayList<>());
+            }
+
+          this.dictionary.get(word).add(translation);
+        }
+
+
+    public ArrayList<String> translate(String word){
+            if (dictionary.containsKey(word)){
+                return dictionary.get(word);
+            }
+            return new ArrayList<>();
+    }
+
+
+
+    public void remove(String word){
+      this.dictionary.remove(word);
+
+    }
+    }
+    class MainDictionary {
+        public static void main(String[] args) {
+
+            DictionaryOfManyTranslations dictionary = new DictionaryOfManyTranslations();
+            dictionary.add("lie", "maata");
+            dictionary.add("lie", "valehdella");
+
+            dictionary.add("bow", "jousi");
+            dictionary.add("bow", "kumartaa");
+
+            System.out.println(dictionary.translate("lie"));
+            dictionary.remove("bow");
+            System.out.println(dictionary.translate("bow"));
+        }
+    }
+
