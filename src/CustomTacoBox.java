786896b91be3a6784471836fdb4565ca75f5cffb
margaretnjoki <maggykiroko@gmail.com>
2026-06-26 16:03:33 +0300

    taco boxes

---
+++ src/CustomTacoBox.java
@@ -0,0 +1,26 @@
+public class CustomTacoBox implements TacoBox{
+        private int tacos;
+
+    public CustomTacoBox(int tacos) {
+        this.tacos=tacos;
+
+    }
+
+    @Override
+    public int tacosRemaining() {
+       return this.tacos;
+
+    }
+
+    @Override
+    public void eat() {
+        if ((this.tacos -1) < 0){
+            return;
+
+        }
+        this.tacos= this.tacos - 1;
+
+
+
+    }
+}
